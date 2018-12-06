45ae761d679ef8be3fd5e5a4a6cf5b03acb19258
zhaoxiaoer <445774833@qq.com>
2018-12-06 19:15:27 +0800

    使用java方式配置Spring MVC

---
+++ src/com/nnniu/bs/ch3/AppConfig.java
@@ -0,0 +1,21 @@
+package com.nnniu.bs.ch3;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan(basePackages = {"com.nnniu.bs.ch3"})
+public class AppConfig {
+
+	@Bean
+	public InternalResourceViewResolver getInternalResourceViewResolver() {
+		InternalResourceViewResolver resolver = 
+				new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/pages/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+}
