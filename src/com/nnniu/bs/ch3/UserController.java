72ba400a8c4f1b12de3c96a03c9fbd6795e253e4
zhaoxiaoer <445774833@qq.com>
2018-12-11 14:40:42 +0800

    调试@ModelAttribute和@PathVariable注解

--- src/com/nnniu/bs/ch3/UserController.java
+++ src/com/nnniu/bs/ch3/UserController.java
@@ -1,6 +1,8 @@
 package com.nnniu.bs.ch3;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,9 +17,14 @@ public class UserController {
 		User user = new User();
 		user.setName("zhao");
 		ModelAndView modelAndView = new ModelAndView("userForm", "user", user);
-		modelAndView.addObject("genders", Gender.values());
+//		modelAndView.addObject("genders", Gender.values());
 		modelAndView.addObject("countries", countries);
 		return modelAndView;
+	}
+	
+	@ModelAttribute("genders")
+	public Gender[] genders() {
+		return Gender.values();
 	}
 	
 	@RequestMapping(value="/result")
@@ -25,6 +32,13 @@ public class UserController {
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("userResult");
 		modelAndView.addObject("u", user);
+		return modelAndView;
+	}
+	
+	@RequestMapping("/testPathVariable/{name}")
+	public ModelAndView testPathVariable(@PathVariable("name") String n) {
+		ModelAndView modelAndView = new ModelAndView("testPathVariable");
+		
 		return modelAndView;
 	}
 
