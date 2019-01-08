30c1282bf8855f258bd4bd1dd1a57c22a6a073c2
zhaoxiaoer <445774833@qq.com>
2019-01-08 11:35:51 +0800

    log4j2的配置改为xml格式

--- src/com/nnniu/bh/ch3/Main.java
+++ src/com/nnniu/bh/ch3/Main.java
@@ -1,6 +1,8 @@
 package com.nnniu.bh.ch3;
 
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -14,9 +16,7 @@ public class Main {
 	private static Logger logger = LogManager.getLogger(Main.class);
 	
 	
-	public static void main(String[] args) {
-		logger.error("1111111");
-		
+	public static void main(String[] args) {		
 		SessionFactory sessionFactory = new Configuration().
 				configure("com/nnniu/bh/ch3/hibernate.cfg.xml").buildSessionFactory();
 		Session session = sessionFactory.openSession();
@@ -24,6 +24,16 @@ public class Main {
 		Message msg = new Message("Hibernated a message on " + new Date());
 		session.save(msg);
 		session.getTransaction().commit();
+		session.close();
+		
+		session = sessionFactory.openSession();
+		List messages = session.createQuery("from Message").list();
+		logger.debug("Found " + messages.size() + "message(s):");
+		Iterator i = messages.iterator();
+		while (i.hasNext()) {
+			Message msg2 = (Message) i.next();
+			logger.debug(msg2.getMessage());
+		}
 		session.close();
 	}
 	
