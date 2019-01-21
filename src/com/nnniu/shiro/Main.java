c5dfc0080f5f7292289d8f14983a7985df4ff7d5
zhaoxiaoer <445774833@qq.com>
2019-01-21 15:12:58 +0800

    调试shiro，使用ini配置文件

---
+++ src/com/nnniu/shiro/Main.java
@@ -0,0 +1,50 @@
+package com.nnniu.shiro;
+
+import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+	// log4j原始API
+//	private static Logger logger = LogManager.getLogger(Main.class);
+	// slf4j API
+	private static Logger logger = LoggerFactory.getLogger(Main.class);
+	
+	public static void main(String[] args) {
+		// 1. 获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+//		Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+		Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro-realm.ini");
+		
+		// 2. 得到SecurityManager实例，并绑定给SecurityUtils
+		org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+		SecurityUtils.setSecurityManager(securityManager);
+		
+		// 3. 得到Subject，创建用户名/密码身份验证Token （用户身份/凭证）
+		Subject subject = SecurityUtils.getSubject();
+		UsernamePasswordToken token = new UsernamePasswordToken("zhao", "123");
+		
+		try {
+			logger.debug("aaaaaaaa:");
+			// 4. 登录，即身份验证
+			subject.login(token);
+		} catch (AuthenticationException e) {
+			// 5. 身份验证失败
+			logger.debug("bbbbbbbbb: " + e.toString());
+		}
+		
+		// 6. 用户已经登录
+		
+		// 7. 退出
+		subject.logout();
+	}
+	
+}
