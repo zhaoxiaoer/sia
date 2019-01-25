6e6f106580d455e5e7e4b9873730a8ca428e76de
zhaoxiaoer <445774833@qq.com>
2019-01-25 15:52:26 +0800

    测试base64编解码，测试MD5,SHA-256等散列算法，测试AES加解密算法

--- src/com/nnniu/shiro/ch3/Main2.java
+++ src/com/nnniu/shiro/ch3/Main2.java
@@ -1,5 +1,6 @@
 package com.nnniu.shiro.ch3;
 
+import java.security.Key;
 import java.util.Arrays;
 import java.util.Iterator;
 
@@ -11,9 +12,18 @@ import org.apache.shiro.authc.pam.ModularRealmAuthenticator;
 import org.apache.shiro.authz.ModularRealmAuthorizer;
 import org.apache.shiro.authz.UnauthorizedException;
 import org.apache.shiro.authz.permission.WildcardPermissionResolver;
+import org.apache.shiro.codec.Base64;
+import org.apache.shiro.codec.Hex;
+import org.apache.shiro.crypto.AesCipherService;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.DefaultHashService;
+import org.apache.shiro.crypto.hash.HashRequest;
+import org.apache.shiro.crypto.hash.Md5Hash;
 import org.apache.shiro.mgt.DefaultSecurityManager;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.ByteSource;
+import org.apache.shiro.util.SimpleByteSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -78,6 +88,58 @@ public class Main2 {
 		} catch (UnauthorizedException e) {
 			logger.error("111: " + e.toString());
 		}
+		
+		// Base64编码解码
+		String str = "hello";
+		String base64Encoded = Base64.encodeToString(str.getBytes());
+		String str2 = Base64.decodeToString(base64Encoded);
+		logger.debug("str2: " + str2);
+		
+		// 16进制编码解码
+		str = "hello2";
+		String hexEncoded = Hex.encodeToString(str.getBytes());
+		str2 = new String(Hex.decode(hexEncoded.getBytes()));
+		logger.debug("str2: " + str2);
+		
+		// 散列转换
+		str = "hello";
+		String salt = "123";
+		String md5 = new Md5Hash(str, salt).toString();
+		String md52 = new Md5Hash(str + salt).toString();
+		String md53 = new Md5Hash(salt + str).toString();
+		logger.debug("md5: " + md5 + ", md52: " + md52 + ", md53: " + md53);
+		
+		// 默认算法SHA-512
+		DefaultHashService hashService = new DefaultHashService();
+		hashService.setHashAlgorithmName("SHA-512");
+		// 私盐，默认无
+		hashService.setPrivateSalt(new SimpleByteSource("123"));
+		// 是否生成公盐，默认false
+		hashService.setGeneratePublicSalt(true);
+		// 用于生成公盐的随机数产生器
+		hashService.setRandomNumberGenerator(new SecureRandomNumberGenerator());
+		// 生成Hash值的迭代次数
+		hashService.setHashIterations(1);
+		HashRequest request = new HashRequest.Builder()
+				.setAlgorithmName("MD5").setSource(ByteSource.Util.bytes("hello"))
+				.setSalt(ByteSource.Util.bytes("123")).setIterations(1).build();
+		String md5Str = hashService.computeHash(request).toString();
+		String md5Str2 = new Md5Hash("123" + "123" + "hello").toString();
+		logger.debug("md5Str: " + md5Str + ", md5Str2: " + md5Str2);
+		
+		// 对称式加密/解密算法
+		// AES算法实现
+		AesCipherService aesCipherService = new AesCipherService();
+		// 设置key长度
+		aesCipherService.setKeySize(128);
+		// 生成key
+		Key key = aesCipherService.generateNewKey();
+		String text = "hello";
+		// 加密
+		String encText = aesCipherService.encrypt(text.getBytes(), key.getEncoded()).toHex();
+		// 解密
+		String decText = new String(aesCipherService.decrypt(Hex.decode(encText.getBytes()), key.getEncoded()).getBytes());
+		logger.debug("encText: " + encText + ", decText: " + decText);
 	}
 
 }
